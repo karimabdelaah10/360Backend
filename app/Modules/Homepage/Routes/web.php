8fc412240974d22a293df9edfe7e050fb7933881
karimabdelaah10 <40154084+karimabdelaah10@users.noreply.github.com>
2021-05-30 18:19:41 +0200

    adding clear routes

--- app/Modules/Homepage/Routes/web.php
+++ app/Modules/Homepage/Routes/web.php
@@ -1,4 +1,11 @@
 <?php
 Route::group([], function () {
     Route::get('/', 'HomepageController@getIndex');
+    Route::get('/clear',function(){
+        Artisan::call('view:clear');
+        Artisan::call('route:clear');
+        Artisan::call('config:cache');
+        Artisan::call('cache:clear');
+        return "clear done";
+    });
 });
