5c24d78c4783d0d624daa77f0ec1c3e232bb9174
azaian <abdalmoniem.s.zaian@gmail.com>
2021-11-26 18:22:24 +0200

    zaian

--- app/Modules/Project/Views/Web/Components/nextProject.blade.php
+++ app/Modules/Project/Views/Web/Components/nextProject.blade.php
@@ -1,28 +1,54 @@
+<div class="c-col-3 hide-mobile"></div>
+
+<!-- Column -->
+<div class="c-col-3 no-gap">
+    <a href="{{url('project',$component->Fields[0]->value)}}">
+        <!-- Image Wrapper -->
+        <div class="image-wrapper has-animation slide-right">
+            <div class="vertical-item-title image-title ">
+                            <span class="title-line">
+  {{$component->getProjectById($component->Fields[0]->value)->name}}
+                            </span>
+            </div>
+            <img src="{{image($component->getProjectById($component->Fields[0]->value)->image,'large')}}"
+                 alt="Single Image">
+        </div>
+        <!--/ Image Wrapper -->
+    </a>
+</div>
+<!--/ Column -->
+
+<div class="c-col-3 hide-mobile"></div>
+
+
 <!-- Next Project Setion -->
 {{--<div class="projects-nav">--}}
-    <div class="next-project">
-        <!-- Next Project URL -->
-        <a href="{{url('project',$component->Fields[0]->value)}}">
-            <div class="next-project-wrapper">
+{{--    <div class="next-project">--}}
+{{--        <!-- Next Project URL -->--}}
+{{--        <a href="{{url('project',$component->Fields[0]->value)}}">--}}
+{{--            <div class="next-project-wrapper">--}}
 
-                <span class="next-project-span has-animation skew-up">Next Project</span>
+{{--                <span class="next-project-span has-animation skew-up">Next Project</span>--}}
 
-                <!-- Next Project Title -->
-                <h1 class="next-project-title has-animation skew-up">
-                    {{$component->getProjectById($component->Fields[0]->value)->name}}</h1>
-                <!--/ Next Project Title -->
-            </div>
+{{--                <!-- Next Project Title -->--}}
+{{--                <h1 class="next-project-title has-animation skew-up">--}}
+{{--                    {{$component->getProjectById($component->Fields[0]->value)->name}}</h1>--}}
+{{--                <!--/ Next Project Title -->--}}
+{{--            </div>--}}
 
-            <!-- Next Project Image -->
-            <div class="next-project-image-wrapper">
-                <img src="{{image($component->getProjectById($component->Fields[0]->value)->image,'large')}}" alt="Next Project Image">
-            </div>
-            <!--/ Next Project Image -->
+{{--            <!-- Next Project Image -->--}}
+{{--            <div class="next-project-image-wrapper">--}}
+{{--                <img src="{{image($component->getProjectById($component->Fields[0]->value)->image,'large')}}"--}}
+{{--                     alt="Next Project Image">--}}
+{{--            </div>--}}
+{{--            <!--/ Next Project Image -->--}}
 
-        </a>
-        <!--/ Next Project URL -->
-    </div>
+{{--        </a>--}}
+{{--        <!--/ Next Project URL -->--}}
+{{--    </div>--}}
 
 
 {{--</div>--}}
 <!--/ Next Project Setion ends -->
+
+
